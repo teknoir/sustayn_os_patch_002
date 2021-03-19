ee7e8a57c0a1b1cce62ed1c3ae777bd775bc86da
Anders Åslund <anders.aslund@teknoir.ai>
2021-03-19 15:27:16 +0100

    Released patch 002

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,8 +11,8 @@ tmp_dir=$(mktemp -d -t sustayn-patch-002XXX)
 pushd $tmp_dir
 echo "Applying patch!"
 
-$SUDO /usr/bin/python3 -m pip install https://github.com/tekn0ir/timeloop/releases/download/v1.0.2/timeloop-1.0.2.tar.gz --upgrade
-$SUDO wget -O /usr/local/lib/keepup_wwan/keepup_wwan.py https://github.com/tekn0ir/timeloop/releases/download/v1.0.2/keepup_wwan.py
+$SUDO /usr/bin/python3 -m pip install https://github.com/teknoir/sustayn_os_patch_002/releases/download/patch002/timeloop-1.0.2.tar.gz --upgrade
+$SUDO wget -O /usr/local/lib/keepup_wwan/keepup_wwan.py https://github.com/teknoir/sustayn_os_patch_002/releases/download/patch002/keepup_wwan.py
 $SUDO systemctl restart keepup_wwan@wwan0.service
 
 echo "Patch applied successfully!"
